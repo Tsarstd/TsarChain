736733ef97928cf1f2a32e4861d301c1c5690f18
Tsar Studio <muh.caesardwi@gmail.com>
2025-10-07 20:55:50 +0700

    fixing logging

--- apps/light_wallet.py
+++ apps/light_wallet.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2025 Tsar Studio
 # Part of TsarChain — see LICENSE and TRADEMARKS.md
 # Refs: BIP141; BIP173
+
 # ---------------- Imports (Module) ----------------
 import os
 import sys
